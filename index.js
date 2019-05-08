d102ee5c898df44747795bbd18f077b40ee7dcd6
Andrew Zhang <alz2@illinois.edu>
2019-05-08 00:57:16 -0500

    Added GH link and picture click

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let aboutMeParagraph = document.getElementById("about-me");
 fetch("/static/me.txt")
     .then(r => r.text())
     .then(d => aboutMeParagraph.textContent = d);
+
+// bio button click
+var normal = true;
+let bioPic = document.getElementById("bio-pic");
+bioPic.onclick = () => {
+    normal = !normal;
+    bioPic.src = normal ? "/images/me.jpg" : "/images/me2.jpg";
+}
+
+// link buttons
+let ghButton = document.getElementById("gh-btn");
+console.log(ghButton);
+ghButton.onclick = () => { window.open('https://www.github.com/alz2', '_blank') };
