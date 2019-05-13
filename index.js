3dc966e0087b6c297403f808c42fd90322c8a6fd
Andrew Zhang <alz2@illinois.edu>
2019-05-13 00:30:27 -0500

    Added Resume Button

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ bioPic.onclick = () => {
 }
 
 // link buttons
+let ghUrl = 'https://www.github.com/alz2';
 let ghButton = document.getElementById("gh-btn");
-console.log(ghButton);
-ghButton.onclick = () => { window.open('https://www.github.com/alz2', '_blank') };
+ghButton.onclick = () => { window.open(ghUrl, '_blank') };
+
+let resumeUrl = 'https://drive.google.com/file/d/1fABcEKgHp-9dXZGNe8BKLDGa5c4MTJK2/view?usp=sharing';
+let resumeButton = document.getElementById("resume-btn");
+resumeButton.onclick = () => { window.open(resumeUrl, '_blank') }
+
