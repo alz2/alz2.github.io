b2fac0fd24facf8bf684bf19f63dbd892954cab8
Andrew Zhang <alz2@illinois.edu>
2020-09-04 23:54:52 -0500

    remove site

--- _site/index.js
+++
@@ -1,9 +0,0 @@
-window.alert('hello');
-
-// Modulate Profile Picture on click.
-var normal_profile_pic = true;
-let bio_img = document.getElementById("bio-pic");
-bio_img.onclick = () => {
-    normal_profile_pic = !normal_profile_pic;
-    bio_img.src = normal_profile_pic ? "assets/images/profile.jpg" : "assets/images/profile2.jpg";
-}
